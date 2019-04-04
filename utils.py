592e947aa440b771a8e772fefb1b2a34ff3755d9
dev-fahim <fahim6668@gmail.com>
2019-04-04 10:27:02 +0600

    Testing wit

--- utils.py
+++ utils.py
@@ -3,44 +3,10 @@ from gnewsclient import gnewsclient
 
 access_token = "VUUDTV5DMUZQGVA66ANKETHB7BCSQ26U"
 
-client = Wit(access_token=access_token)
+client = Wit(access_token = access_token)
 
+message_text = "I live in canada"
 
-def wit_response(message_text):
-    resp = client.message(message_text)
-    categories = {'newstype': None, 'location': None}
+resp = client.message(message_text)
 
-    entities = list(resp['entities'])
-    for entity in entities:
-        categories[entity] = resp['entities'][entity][0]['value']
-
-    return categories
-
-
-def get_news_elements(categories):
-    news_client = gnewsclient()
-    news_client.query = ''
-
-    if categories['newstype'] != None:
-        news_client.query += categories['newstype'] + ' '  # for print result here will be a space
-
-    if categories['location'] != None:
-        news_client.query += categories['location']
-
-    news_items = news_client.get_news()
-
-    elements = []
-
-    for item in news_items:
-        element = {
-            'title': item['title'],
-            'buttons': [{
-                'type': 'web_url',
-                'title': "Read more",
-                'url': item['link']
-            }],
-            'image_url': item['img']
-        }
-        elements.append(element)
-
-    return elements
+print(resp)
